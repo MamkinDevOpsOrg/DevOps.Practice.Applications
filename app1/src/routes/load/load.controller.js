3ce9545b38b12b67714a6e6bdf47ef13b681da8e
Artsiom-Kapset <Artsiom.Kapset@puregym.com>
2025-04-15 15:25:30 +0200

    feat: dynamic cpu_utilization value added

--- app1/src/routes/load/load.controller.js
+++ app1/src/routes/load/load.controller.js
@@ -1,13 +1,21 @@
 const { logCpuUsage, logMemoryUsage } = require('../../helpers/logs');
 
-// CPU stress (burns ~20-25% of 1 core for 10 seconds)
+// CPU stress
 async function load_cpu(req, res) {
+  Number(req.params.cpu_utilization);
   console.log('[CPU] Start');
 
   const cpuStart = process.cpuUsage();
   const endTime = Date.now() + 10_000;
   const burnInterval = 100;
-  const cpuLoad = 0.25;
+  let cpuLoad = 0.25; // burns ~20-25% of 1 core for 10 seconds
+
+  if (req.params.cpu_utilization) {
+    const cpu_utilization = Number(req.params.cpu_utilization);
+    if (cpu_utilization > 0 && cpu_utilization < 100) {
+      cpuLoad = cpu_utilization / 100; // burns ~(cpu_utilization / 100)% of 1 core for 10 seconds
+    }
+  }
 
   function burnCpu() {
     const now = Date.now();
--- app1/src/routes/load/load.router.js
+++ app1/src/routes/load/load.router.js
@@ -4,6 +4,7 @@ const loadController = require('./load.controller');
 const loadRouter = express.Router();
 
 loadRouter.get('/cpu', loadController.load_cpu);
+loadRouter.get('/cpu/:cpu_utilization', loadController.load_cpu);
 loadRouter.get('/mem', loadController.load_mem);
 
 module.exports = loadRouter;
