b328131bc8ceb0987fb6df6ea04e0bd441b4b566
Artsiom-Kapset <Artsiom.Kapset@puregym.com>
2025-06-20 17:42:15 +0200

    feat(ui): display ECS Task metadata (ID & IP) on main page

--- app1/src/routes/meta/meta.controller.js
+++ app1/src/routes/meta/meta.controller.js
@@ -1,29 +1,32 @@
 const http = require('http');
 
-async function getInstanceMeta(key) {
-  return new Promise((resolve, reject) => {
-    const options = {
-      hostname: '169.254.169.254',
-      path: `/latest/meta-data/${key}`,
-      timeout: 1000,
-    };
+async function getMetadataV4(path) {
+  const options = {
+    hostname: '169.254.170.2',
+    path,
+    timeout: 1000,
+  };
 
+  return new Promise((resolve) => {
     http
       .get(options, (res) => {
         let data = '';
         res.on('data', (chunk) => (data += chunk));
-        res.on('end', () => resolve(data));
+        res.on('end', () => resolve(JSON.parse(data)));
       })
       .on('error', (err) => {
-        console.error(`[Meta] Failed to get ${key}:`, err);
-        resolve('unknown');
+        console.warn('Failed to get ECS metadata', err);
+        resolve({});
       });
   });
 }
 
 async function getInstanceIdentity(req, res) {
-  const id = await getInstanceMeta('instance-id');
-  const ip = await getInstanceMeta('local-ipv4');
+  const metadata = await getMetadataV4('/v4/task');
+
+  const id = metadata.TaskARN?.split('/').pop() || 'unknown';
+  const ip = metadata.Containers?.[0]?.Networks?.[0]?.IPv4Addresses?.[0] || 'unknown';
+
   res.json({ id, ip });
 }
 
